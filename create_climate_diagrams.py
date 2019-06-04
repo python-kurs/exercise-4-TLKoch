396c382de7645c42bb5e068a4ae5525921f051c6
TLKoch <Tiziana.Li@t-online.de>
2019-06-04 13:15:13 +0200

    solution

--- create_climate_diagrams.py
+++ create_climate_diagrams.py
@@ -1,23 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from pathlib import Path
 
+basedir = Path("C:/Users/Kochti/Desktop/Studium/Python/exercise-4-TLKoch/")
+data_dir = basedir / "data"
+
+
+outputdir = basedir / "output"    
+# outputdir.mkdir()
+    
+    
 # Import both data tables into python using pandas. Set the index column to "MESS_DATUM" and parse the column values as dates. [1P]
-garmisch  = 
-zugspitze = 
+garmisch_dir  = data_dir / "produkt_klima_tag_20171010_20190412_01550.txt"
+zugspitze_dir = data_dir / "produkt_klima_tag_20171010_20190412_05792.txt"
+
+garmisch = pd.read_csv(garmisch_dir, parse_dates=["MESS_DATUM"], index_col="MESS_DATUM", sep = ";")
+zugspitze = pd.read_csv(zugspitze_dir, parse_dates=["MESS_DATUM"], index_col="MESS_DATUM", sep = ";")
 
 # Clip the tables to the year 2018: [1P]
-garmisch  = 
-zugspitze = 
+garmisch  = garmisch.loc["2018"]
+zugspitze = zugspitze.loc["2018"]
 
 # Resample the temperature data to monthly averages (" TMK") and store them in simple lists: [1P]
-garmisch_agg  = 
-zugspitze_agg = 
+garmisch_agg  = list(garmisch.loc[:, " TMK"].resample("M").mean())
+zugspitze_agg = list(zugspitze.loc[:, " TMK"].resample("M").mean())
 
 # Define a plotting function that draws a simple climate diagram
 # Add the arguments as mentioned in the docstring below [1P]
 # Set the default temperature range from -15°C to 20°C and the precipitation range from 0mm to 370mm [1P]
-def create_climate_diagram(...):
+def create_climate_diagram(
+        df,
+        temp_col,
+        prec_col,
+        title,
+        filename,
+        temp_min = -15,
+        temp_max = 20,
+        prec_min = 0,
+        prec_max = 370):
     """
     Draw a climate diagram.
     
@@ -48,33 +69,69 @@ def create_climate_diagram(...):
     
     """
 
+
+
     fig = plt.figure(figsize=(10,8))
     plt.rcParams['font.size'] = 16
 
     ax2 = fig.add_subplot(111)
     ax1 = ax2.twinx()
+    
+    days = mdates.DayLocator(bymonthday=28)
+    monthFmt = mdates.DateFormatter("%b")
+    ax2.xaxis.set_major_locator(days)
+    ax2.xaxis.set_major_formatter(monthFmt)
+    ax1.xaxis.set_major_locator(days)
+    ax1.xaxis.set_major_formatter(monthFmt)
+    
+    
+    # Set appropiate limits to each y-axis using the function arguments: [1P]
+    
+    ax2.set_ylim(prec_min, prec_max)
+    ax1.set_ylim(temp_min, temp_max)
+    
+    
 
     # Draw temperature values as a red line and precipitation values as blue bars: [1P]
     # Hint: Check out the matplotlib documentation how to plot barcharts. Try to directly set the correct
-    #       x-axis labels (month shortnames).
-    ax2.bar(...)
-    ax1.plot(...)
+
+
+    # agg sum Regen, mean Temp auf monatlich
     
-    # Set appropiate limits to each y-axis using the function arguments: [1P]
-    ax2.
-    ax1.
+    df = df.loc[:, [temp_col, prec_col]].resample("M").agg({temp_col: "mean", prec_col: "sum"})
+    
+    ax2.bar(x = df.index, height = list(df.loc[:, prec_col]), color = "b", width = 25, label = "Precipitation")
+    ax1.plot(df.loc[:,temp_col], c="r", label = "Temperature")
+
+    
+
+
     
     # Set appropiate labels to each y-axis: [1P]
-    ax2.
-    ax1.
+    ax2.set_ylabel("Precipitation (mm)")
+    ax1.set_ylabel("Temperature (°C)")
 
     # Give your diagram the title from the passed arguments: [1P]
-    plt.title(...)
+    plt.title(title)
 
     # Save the figure as png image in the "output" folder with the given filename. [1P]
+ 
+    plt.savefig(filename)
+    
+    
     
     return fig
 
 # Use this function to draw a climate diagram for 2018 for both stations and save the result: [1P]
-create_climate_diagram(...)
-create_climate_diagram(...)
+create_climate_diagram(df = garmisch,
+                       filename = "garmisch.png", 
+                       temp_col = " TMK", 
+                       prec_col = " RSK", 
+                       title = "Garmisch")
+
+create_climate_diagram(df = zugspitze,
+                       filename = "zugspitze.png", 
+                       temp_col = " TMK", 
+                       prec_col = " RSK", 
+                       title = "Zugspitze")
+
